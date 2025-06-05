a6b11db49a8efb3cc0219f537605e332597ad9ad
thiriondan <daniel.thirion@rte-france.com>
2025-06-05 11:56:23 +0200

    Upgrade to Spring Boot 3.4.6 & Spring Cloud 2024.0.1
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>

--- gridcapa-core-cc-post-processing-app/src/test/java/com/farao_community/farao/core_cc_post_processing/app/services/DailyF303Generator2Test.java
+++ gridcapa-core-cc-post-processing-app/src/test/java/com/farao_community/farao/core_cc_post_processing/app/services/DailyF303Generator2Test.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.io.InputStream;
 import java.time.OffsetDateTime;
@@ -59,7 +59,7 @@ class DailyF303Generator2Test {
 
     @Autowired
     private DailyF303Generator dailyFbConstraintDocumentGenerator;
-    @MockBean
+    @MockitoBean
     private MinioAdapter minioAdapter;
     private final Set<TaskDto> taskDtos = new HashSet<>();
     private final Map<TaskDto, ProcessFileDto> raoResult = new HashMap<>();
